f9a68031a747d3c49dbe3395040f231ee4e0b5f0
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-09-15 10:58:37 +0200

    Replaced firestore with rtdb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,17 @@
 import admin from 'firebase-admin';
-import { PRIVATE_KEY, PROJECT_ID, CLIENT_EMAIL } from '$env/static/private';
+import { PRIVATE_KEY, PROJECT_ID, CLIENT_EMAIL, DATABASE_URL } from '$env/static/private';
 
 const firebaseConfig = {
 	credential: admin.credential.cert({
 		projectId: PROJECT_ID,
 		privateKey: PRIVATE_KEY.replace(/\\n/g, '\n'),
 		clientEmail: CLIENT_EMAIL
-	})
+	}),
+	databaseURL: DATABASE_URL
 };
 
 admin.initializeApp(firebaseConfig);
 const storage = admin.storage();
-const firestore = admin.firestore();
+const database = admin.database();
 
-export { admin, storage, firestore };
+export { admin, storage, database };
--- src/routes/api/recipe/+server.ts
+++ src/routes/api/recipe/+server.ts
@@ -1,12 +1,12 @@
-import { firestore } from '$lib/firebase';
+import { database } from '$lib/firebase';
 import { json } from '@sveltejs/kit';
 
-export async function GET() {
-	const cityRef = firestore.collection('12345').doc('YtAn4V4ErjOrs9Sf5hnm');
-	const doc = await cityRef.get();
-	if (!doc.exists) {
-		return json('No such document!');
-	} else {
-		return json(doc.data());
-	}
+export async function GET({ url }) {
+	const id = url.searchParams.get('recipeId') || '';
+	if (id === '') return new Response('Bad Request', { status: 400 });
+
+	let ref = database.ref('recipes/' + id);
+	const snapshot = await ref.get();
+	const data = snapshot.val();
+	return json(data);
 }
