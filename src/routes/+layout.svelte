e84796a77e99d13c7744832912941c3175091508
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-09-26 10:47:14 +0200

    Added Login/Signup page design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -36,7 +36,35 @@
 			<div class="navbar-center">
 				<a href="/" class="btn btn-ghost normal-case text-xl">Cooking Cache</a>
 			</div>
-			<div class="navbar-end" />
+			<div class="navbar-end">
+				<div class="dropdown dropdown-end">
+					<label tabindex="-2" for="" class="btn btn-ghost btn-circle avatar">
+						<div class="w-6 rounded-full">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+								/>
+							</svg>
+						</div>
+					</label>
+					<ul
+						tabindex="-2"
+						class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
+					>
+						<li><a href="/settings">Settings</a></li>
+						<li><a href="/login">Login</a></li>
+						<li><a href="#logout">Logout</a></li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
 
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	let loggingIn = true;
+</script>
+
+<div class="flex flex-col gap-4">
+	<h1 class="text-2xl font-extrabold text-center mb-2">Werde Mitglied!🥂</h1>
+
+	<div class="join w-full">
+		<input
+			class="btn flex-1 join-item"
+			type="radio"
+			name="options"
+			aria-label="Login"
+			value={true}
+			bind:group={loggingIn}
+		/>
+		<input
+			class="btn flex-1 join-item"
+			type="radio"
+			name="options"
+			aria-label="Signup"
+			value={false}
+			bind:group={loggingIn}
+		/>
+	</div>
+
+	<form class="flex flex-col gap-2">
+		<div class="form-control w-full max-w-s">
+			<label class="label" for="">
+				<span class="label-text">E-Mail</span>
+			</label>
+			<input type="text" placeholder="E-Mail" class="input input-bordered w-full max-w-s" />
+		</div>
+		<div class="form-control w-full max-w-s">
+			<label class="label" for="">
+				<span class="label-text">Passwort</span>
+			</label>
+			<input type="password" placeholder="Passwort" class="input input-bordered w-full max-w-s" />
+		</div>
+
+		{#if !loggingIn}
+			<div class="form-control w-full max-w-s">
+				<label class="label" for="">
+					<span class="label-text">Passwort bestätigen</span>
+				</label>
+				<input type="password" placeholder="*****" class="input input-bordered w-full max-w-s" />
+			</div>
+		{/if}
+
+		<button class="btn btn-primary w-full">
+			{loggingIn ? 'LOGIN' : 'SIGNUP'}
+		</button>
+	</form>
+</div>
