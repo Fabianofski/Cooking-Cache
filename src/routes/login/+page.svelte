c9b4b21cfc923f8327f3f311736c19078c9348bd
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-11-02 21:43:16 +0100

    Added OAuth Provider Login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,7 +3,12 @@
 	import {
 		createUserWithEmailAndPassword,
 		signInWithEmailAndPassword,
-		updateProfile
+		GoogleAuthProvider,
+		FacebookAuthProvider,
+		OAuthProvider,
+		updateProfile,
+		signInWithPopup,
+		type AuthProvider
 	} from 'firebase/auth';
 	import { currentUser } from '../../stores/store';
 	import { goto } from '$app/navigation';
@@ -31,6 +36,24 @@
 		loading = true;
 
 		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				currentUser.set(userCredential.user);
+				goto('/');
+			})
+			.catch((error) => {
+				console.log('Failed: ' + error.message);
+			})
+			.finally(() => {
+				loading = false;
+			});
+	}
+	const googleProvider = new GoogleAuthProvider();
+	const facebookProvider = new FacebookAuthProvider();
+	const appleProvider = new OAuthProvider('apple.com');
+
+	function loginWithOAuth(provider: AuthProvider) {
+		loading = true;
+		signInWithPopup(auth, provider)
 			.then((userCredential) => {
 				currentUser.set(userCredential.user);
 				goto('/');
@@ -153,4 +176,61 @@
 			{/if}
 		</button>
 	</form>
+
+	<div class="grid grid-cols-3 gap-2">
+		<button
+			class="px-4 py-2 border flex gap-2 bg-white border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+			on:click={() => loginWithOAuth(googleProvider)}
+		>
+			<img
+				class="w-6 h-6"
+				src="https://www.svgrepo.com/show/475656/google-color.svg"
+				loading="lazy"
+				alt="google logo"
+			/>
+			<span>Mit Google anmelden</span>
+		</button>
+		<button
+			disabled
+			class="cursor-not-allowed px-4 py-2 border flex gap-2 bg-gray-800 border-slate-700 rounded-lg text-slate-200 hover:border-slate-500 hover:text-slate-300 hover:shadow transition duration-150"
+			on:click={() => loginWithOAuth(facebookProvider)}
+		>
+			<img
+				class="w-6 h-6"
+				src="https://www.svgrepo.com/show/475647/facebook-color.svg"
+				loading="lazy"
+				alt="facebook logo"
+			/>
+			<span>Mit Facebook anmelden</span>
+		</button>
+		<button
+			disabled
+			class="cursor-not-allowed px-4 py-2 border flex gap-2 bg-black border-slate-700 rounded-lg text-slate-200 hover:border-slate-500 hover:text-slate-300 hover:shadow transition duration-150"
+			on:click={() => loginWithOAuth(appleProvider)}
+		>
+			<svg
+				class="w-6 h-6"
+				fill="#ffffff"
+				version="1.1"
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
+				viewBox="0 0 512 512"
+				xml:space="preserve"
+				><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+					id="SVGRepo_tracerCarrier"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/><g id="SVGRepo_iconCarrier">
+					<g id="7935ec95c421cee6d86eb22ecd114eed">
+						<path
+							style="display: inline;"
+							d="M248.644,123.476c-5.45-29.71,8.598-60.285,25.516-80.89 c18.645-22.735,50.642-40.17,77.986-42.086c4.619,31.149-8.093,61.498-24.826,82.965 C309.37,106.527,278.508,124.411,248.644,123.476z M409.034,231.131c8.461-23.606,25.223-44.845,51.227-59.175 c-26.278-32.792-63.173-51.83-97.99-51.83c-46.065,0-65.542,21.947-97.538,21.947c-32.96,0-57.965-21.947-97.866-21.947 c-39.127,0-80.776,23.848-107.19,64.577c-9.712,15.055-16.291,33.758-19.879,54.59c-9.956,58.439,4.916,134.557,49.279,202.144 c21.57,32.796,50.321,69.737,87.881,70.059c33.459,0.327,42.951-21.392,88.246-21.616c45.362-0.258,53.959,21.841,87.372,21.522 c37.571-0.317,67.906-41.199,89.476-73.991c15.359-23.532,21.167-35.418,33.11-62.023 C414.435,352.487,389.459,285.571,409.034,231.131z"
+						/>
+					</g>
+				</g></svg
+			>
+
+			<span>Mit Apple anmelden</span>
+		</button>
+	</div>
 </div>
