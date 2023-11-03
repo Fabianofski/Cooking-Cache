dac6600b10ccd0e45598c86f93ffd7e08b5f82fe
Fabian Friedrich <friedrich.fabian@gmx.net>
2023-11-03 22:26:24 +0100

    Added Account Deletion Modal

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,39 +1,54 @@
 <script lang="ts">
-	import { signOut, type User } from 'firebase/auth';
+	import { signOut, type User, deleteUser } from 'firebase/auth';
 	import { currentUser } from '../../stores/store';
 	import { goto } from '$app/navigation';
 	import { auth } from '$lib/firebase.client';
 
-	let oldPassword: string;
-	let password: string;
-	let repeatPassword: string;
+	let oldPassword: string = '';
+	let password: string = '';
+	let repeatPassword: string = '';
 
 	let user: User | null = null;
 	currentUser.subscribe((value) => {
 		user = value;
 	});
 
-	let loading = false;
+	let loadingPasswordChange = false;
 	function formIsInvalid(oldPassword: string, password: string, repeatPassword: string): boolean {
 		return oldPassword === '' || password === '' || repeatPassword === '';
 	}
 
 	function changePassword() {
-		loading = true;
+		loadingPasswordChange = true;
+	}
+
+	let loadingDeletion = false;
+	let confirmation = '';
+	let dialog: HTMLDialogElement;
+	function deleteAccount() {
+		loadingDeletion = true;
+		if (user)
+			deleteUser(user).then(() => {
+				goto('login');
+			});
 	}
 
 	// if (!user) goto('/login');
 </script>
 
-<div class="flex flex-col gap-4">
-	<div class="flex px-4 justify-evenly items-center">
+<div class="flex flex-col gap-8">
+	<div class="flex px-4 justify-evenly gap-4 items-center">
 		<div class="avatar">
 			<div class="w-36 rounded-full">
-				<img src={user?.photoURL} alt="Profile" />
+				{#if user && user.photoURL}
+					<img src={user.photoURL} alt="Profile" />
+				{:else}
+					<img src={'/default-cover.jpg'} alt="Default Profile" />
+				{/if}
 			</div>
 		</div>
 
-		<h1 class="align-middle text-2xl font-bold">
+		<h1 class="align-middle text-center text-2xl font-bold">
 			{user?.displayName}
 		</h1>
 	</div>
@@ -98,9 +113,9 @@
 			<button
 				class="btn btn-primary w-full"
 				on:click={changePassword}
-				disabled={loading || formIsInvalid(oldPassword, password, repeatPassword)}
+				disabled={loadingPasswordChange || formIsInvalid(oldPassword, password, repeatPassword)}
 			>
-				{#if !loading}
+				{#if !loadingPasswordChange}
 					Passwort ändern
 				{:else}
 					<span class="loading loading-spinner loading-md" />
@@ -133,23 +148,77 @@
 				Abmelden</button
 			>
 
-			<button class="btn btn-outline btn-error">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-					/>
-				</svg>
-				Konto löschen</button
+			<button
+				class="btn btn-outline btn-error"
+				disabled={loadingDeletion}
+				on:click={() => {
+					dialog.showModal();
+				}}
 			>
+				{#if !loadingDeletion}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+						/>
+					</svg>
+					Konto löschen
+				{:else}
+					<span class="loading loading-spinner loading-md" />
+				{/if}
+			</button>
+			<dialog bind:this={dialog} class="modal">
+				<div class="modal-box">
+					<h3 class="font-bold text-lg text-center">Willst du deinen Account wirklich löschen?</h3>
+					<p class="py-4 text-center">Tippe zum Bestätigen LÖSCHEN ein</p>
+					<div class="form-control w-full max-w-s">
+						<input
+							type="text"
+							placeholder="LÖSCHEN"
+							bind:value={confirmation}
+							class="input input-bordered input-error w-full max-w-s"
+							required
+						/>
+					</div>
+					<div class="modal-action">
+						<form method="dialog" class="w-full">
+							<button
+								class="btn btn-error btn-block"
+								disabled={loadingDeletion || confirmation.toLowerCase() !== 'löschen'}
+								on:click={deleteAccount}
+							>
+								{#if !loadingDeletion}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-6 h-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+										/>
+									</svg>
+									Konto löschen
+								{:else}
+									<span class="loading loading-spinner loading-md" />
+								{/if}
+							</button>
+						</form>
+					</div>
+				</div>
+			</dialog>
 		</div>
 	</div>
 </div>
