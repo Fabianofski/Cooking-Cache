f386bd0290bb9489db29754c4e2c3f3c373b5d2c
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-12-26 14:25:17 +0100

    Added check for permission when posting recipe

--- src/routes/api/recipe/+server.ts
+++ src/routes/api/recipe/+server.ts
@@ -2,6 +2,7 @@ import { database, verifyIdToken, uploadFileToStorage } from '$lib/firebase.admi
 import { json } from '@sveltejs/kit';
 import { v4 as uuidv4 } from 'uuid';
 import type { Recipe } from '../../../models/Recipe.js';
+import type { RecipeCollection } from '../../../models/RecipeCollections.js';
 
 export async function POST({ request }) {
 	const token = request.headers.get('Authorization');
@@ -11,9 +12,18 @@ export async function POST({ request }) {
 
 		const formData = await request.formData();
 		const recipe = JSON.parse(formData.get('recipe') as string) as Recipe;
-		const cover = formData.get('cover') as File;
-		recipe.id = uuidv4();
+		if (!recipe) return new Response('400 Bad Request', { status: 400 });
 
+		const data = await database.ref(`collections/${recipe.collectionId}`).get();
+		const collection: RecipeCollection = data.val() || {};
+		if (!collection) return new Response('404 Not Found', { status: 404 });
+
+		const participants = collection.participants || [];
+		if (!participants.find((p) => p.uid === uid))
+			return new Response('403 Forbidden', { status: 403 });
+
+		recipe.id = uuidv4();
+		const cover = formData.get('cover') as File;
 		if (cover)
 			recipe.image = await uploadFileToStorage(
 				cover,
