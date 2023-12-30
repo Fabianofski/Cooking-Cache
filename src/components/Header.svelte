d1908d86d73283d3bedc80dee26487bb38aeabb8
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2023-12-30 15:53:39 +0100

    Added header dropdown collapse on unfocus

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,6 +4,18 @@
 	export let backLink: string | undefined = undefined;
 	export let sticky: boolean = false;
 	export let options: { callback: () => void; title: string; icon: string | undefined }[] = [];
+
+	let justFocussed = false;
+	let dropdown: HTMLElement;
+	function click() {
+		if (!justFocussed) {
+			dropdown.blur();
+		} else justFocussed = false;
+	}
+
+	function focusIn() {
+		justFocussed = true;
+	}
 </script>
 
 <div class={`w-full bg-base-100 ${sticky ? 'sticky top-16 z-10' : ''}`}>
@@ -36,8 +48,16 @@
 		{/if}
 
 		<div class="absolute top-1 right-0">
-			<details class="dropdown dropdown-end">
-				<summary class="btn btn-ghost btn-xs">
+			<div class="dropdown dropdown-end">
+				<div
+					tabindex="-1"
+					role="button"
+					class="btn btn-ghost btn-xs"
+					bind:this={dropdown}
+					on:click={click}
+					on:keydown={click}
+					on:focus={focusIn}
+				>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -52,20 +72,28 @@
 							d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
 						/>
 					</svg>
-				</summary>
-				<ul class="py-2 shadow menu dropdown-content z-[1] bg-base-200 rounded w-52">
-					{#each options as option}
-						<button class="btn btn-ghost min-h-0 h-8 flex justify-start" on:click={option.callback}>
-							{#if option.icon}<img
-									class="h-5"
-									src={option.icon}
-									alt={option.title + 'Icon'}
-								/>{/if}
-							{option.title}
-						</button>
-					{/each}
+				</div>
+				<ul
+					tabindex="-1"
+					class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+				>
+					<ul class="py-2 shadow menu dropdown-content z-[1] bg-base-200 rounded w-52">
+						{#each options as option}
+							<button
+								class="btn btn-ghost min-h-0 h-8 flex justify-start"
+								on:click={option.callback}
+							>
+								{#if option.icon}<img
+										class="h-5"
+										src={option.icon}
+										alt={option.title + 'Icon'}
+									/>{/if}
+								{option.title}
+							</button>
+						{/each}
+					</ul>
 				</ul>
-			</details>
+			</div>
 		</div>
 	</div>
 	<div class="divider my-0" />
