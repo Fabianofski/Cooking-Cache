a6e0420c66597fa434e464e284c30e90274cb2d1
Fabian Friedrich <friedrich.fabian@gmx.net>
2024-01-13 11:55:50 +0100

    Updated configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const staticSetup = adapterStatic({
+	pages: 'build',
+	assets: 'build',
+	fallback: 'index.html',
+	precompress: false,
+	strict: true
+});
+const autoSetup = adapterAuto({});
+const adapter = process.env.ADAPTER === 'static' ? staticSetup : autoSetup;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,18 +19,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter
 	}
 };
 
