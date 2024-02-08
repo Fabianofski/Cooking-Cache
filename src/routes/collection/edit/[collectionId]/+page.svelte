fe44d967b2a645814d191baab08db68436de7a16
Fabian <friedrich.fabian@gmx.net>
2024-02-08 18:27:19 +0100

    Replaced cover replace btn with dropdown to select or upload img

--- src/routes/collection/edit/[collectionId]/+page.svelte
+++ src/routes/collection/edit/[collectionId]/+page.svelte
@@ -95,7 +95,10 @@
 	}
 
 	let loadingCoverReplacement: boolean = false;
+	let imgReplaceDropdown: HTMLDetailsElement;
 	async function replaceCoverImage(event: Event) {
+		imgReplaceDropdown.open = false;
+
 		const file = (event.target as HTMLInputElement).files?.[0];
 		if (!file || !user) return;
 
@@ -141,35 +144,76 @@
 						accept="image/*"
 						on:change={replaceCoverImage}
 					/>
-					<button
-						class="btn btn-primary btn-circle"
-						disabled={loadingCoverReplacement}
-						on:click={openFileSelection}
-					>
-						{#if loadingCoverReplacement}
-							<span class="loading loading-spinner loading-md" />
-						{:else}
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
-								/>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
-								/>
-							</svg>
-						{/if}
-					</button>
+					<details class="dropdown dropdown-end" bind:this={imgReplaceDropdown}>
+						<summary
+							class="m-1 btn btn-primary btn-circle"
+							class:btn-disabled={loadingCoverReplacement}
+						>
+							{#if loadingCoverReplacement}
+								<span class="loading loading-spinner loading-md" />
+							{:else}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+									/>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
+									/>
+								</svg>
+							{/if}
+						</summary>
+						<ul class="mt-2 p-2 shadow menu dropdown-content z-[1] bg-base-200 rounded-box w-42">
+							<li>
+								<button on:click={openFileSelection}>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-5 h-5"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+										/>
+									</svg>
+									Hochladen
+								</button>
+							</li>
+							<li>
+								<button on:click>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-5 h-5"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59"
+										/>
+									</svg>
+									Auswählen
+								</button>
+							</li>
+						</ul>
+					</details>
 				</div>
 			{/if}
 		</div>
