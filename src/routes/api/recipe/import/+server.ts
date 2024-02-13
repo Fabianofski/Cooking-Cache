5321b61242d3bf08455c547dccfd6c459c5a44f9
Fabian <friedrich.fabian@gmx.net>
2024-02-13 20:52:29 +0100

    Added cheerio and basic instagram import handler

--- src/routes/api/recipe/import/+server.ts
+++ src/routes/api/recipe/import/+server.ts
@@ -1,13 +1,17 @@
 import { json } from '@sveltejs/kit';
 import type { Recipe } from '../../../../models/Recipe.js';
 import { extractChefkochRecipe } from '$lib/server/recipeImport/chefkoch.import.js';
+import { extractInstagramRecipe } from '$lib/server/recipeImport/instagram.import.js';
 
 export async function GET({ url }) {
 	const importURL = url.searchParams.get('url');
 
 	if (!importURL) return new Response('400 Bad Request', { status: 400 });
 
-	const recipe: Recipe = await extractChefkochRecipe(importURL);
+	let recipe: Recipe;
+	if (importURL.includes('chefkoch.de')) recipe = await extractChefkochRecipe(importURL);
+	else if (importURL.includes('instagram.com')) recipe = await extractInstagramRecipe(importURL);
+	else return new Response('400 Bad Request', { status: 400 });
 
 	return json(recipe);
 }
