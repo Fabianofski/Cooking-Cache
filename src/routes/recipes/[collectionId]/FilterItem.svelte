0b9263a0b600dd6bfd5c0d499e2c98cbe89121f7
Fabian <friedrich.fabian@gmx.net>
2024-02-25 18:36:59 +0100

    Added recipe filter: Author

---
+++ src/routes/recipes/[collectionId]/FilterItem.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	export let filterItem: string;
+	export let displayText: string = filterItem;
+	export let filterIcon: string | undefined = undefined;
+	export let checked: boolean;
+	export let onFilterChange: (checked: boolean, value: string) => void;
+</script>
+
+<label class="swap mx-1">
+	<input
+		type="checkbox"
+		{checked}
+		on:input={(e) => {
+			// @ts-ignore
+			onFilterChange(e.target?.checked, filterItem);
+		}}
+	/>
+	<div class="swap-on">
+		<div class="badge badge-neutral">
+			{#if filterIcon}
+				<img src={filterIcon} alt={displayText} class="mr-1 w-4 h-4" />
+			{/if}
+			{displayText}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="ml-1 w-4 h-4"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+				/>
+			</svg>
+		</div>
+	</div>
+	<div class="swap-off">
+		<div class="badge badge-outline">
+			{#if filterIcon}
+				<img src={filterIcon} alt={displayText} class="mr-1 w-4 h-4" />
+			{/if}
+			{displayText}
+		</div>
+	</div>
+</label>
--- src/routes/recipes/[collectionId]/FilterModal.svelte
+++ src/routes/recipes/[collectionId]/FilterModal.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
-	import type { Recipe } from "../../../models/Recipe";
+	import type { Recipe } from '../../../models/Recipe';
+	import type { Participant } from '../../../models/RecipeCollections';
+	import { recipeCollectionsStore } from '../../../stores/recipeCollectionsStore';
+	import FilterItem from './FilterItem.svelte';
 
-    export let filters: string[] = [];
-    export let recipes: Recipe[] = [];
-    export let recipesCount: number = 0;
-    export let filterModal: HTMLDialogElement;
-    export let onFilterChange: (checked: boolean, value: string) => void;
+	export let filters: string[] = [];
+	export let recipes: Recipe[] = [];
+	export let recipesCount: number = 0;
+	export let filterModal: HTMLDialogElement;
+	export let onFilterChange: (checked: boolean, value: string) => void;
+
+	function getDisplayNameByUid(uid: string) {
+		return $recipeCollectionsStore[recipes[0].collectionId].participants?.find(
+			(participant: Participant) => participant.uid === uid
+		)?.displayName;
+	}
+
+	function getPhotoURLByUid(uid: string) {
+		return $recipeCollectionsStore[recipes[0].collectionId].participants?.find(
+			(participant: Participant) => participant.uid === uid
+		)?.photoURL;
+	}
 </script>
 
 <dialog bind:this={filterModal} class="modal">
@@ -18,36 +33,22 @@
 				<div class="divider my-0" />
 				<div>
 					{#each new Set(recipes.flatMap((recipe) => recipe.tags || [])) as filterItem}
-						<label class="swap mx-1">
-							<input
-								type="checkbox"
-								checked={filters.includes(filterItem)}
-								on:input={(e) => {
-									// @ts-ignore
-									onFilterChange(e.target?.checked, filterItem);
-								}}
-							/>
-							<div class="swap-on">
-								<div class="badge badge-neutral">
-									{filterItem}
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke-width="1.5"
-										stroke="currentColor"
-										class="ml-1 w-4 h-4"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-										/>
-									</svg>
-								</div>
-							</div>
-							<div class="swap-off"><div class="badge badge-outline">{filterItem}</div></div>
-						</label>
+						<FilterItem {filterItem} checked={filters.includes(filterItem)} {onFilterChange} />
+					{/each}
+				</div>
+			</div>
+			<div>
+				<h4 class="text-md">Autoren</h4>
+				<div class="divider my-0" />
+				<div>
+					{#each new Set(recipes.flatMap((recipe) => recipe.creatorId || [])) as uid}
+						<FilterItem
+							displayText={getDisplayNameByUid(uid) || ''}
+							filterItem={uid}
+							checked={filters.includes(uid)}
+							{onFilterChange}
+							filterIcon={getPhotoURLByUid(uid) || undefined}
+						/>
 					{/each}
 				</div>
 			</div>
