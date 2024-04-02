f503c03a51ba42d17601f19334aa6c335602f7d1
Fabian <friedrich.fabian@gmx.net>
2024-04-02 19:28:11 +0200

    Fixed wrong bring import link

--- src/routes/recipe/[collectionId]/[id]/RecipePage.svelte
+++ src/routes/recipe/[collectionId]/[id]/RecipePage.svelte
@@ -22,7 +22,7 @@
 		const bringBtn = document.getElementById('bringBtn');
 		if (!bringBtn) return;
 
-		const url = `https://cooking-cache.web.app/recipe/${recipe?.collectionId}/${recipe?.id}/share&key=${recipe?.accessToken}`;
+		const url = `https://cooking-cache.web.app/recipe/${recipe?.collectionId}/${recipe?.id}/share?key=${recipe?.accessToken}`;
 
 		// @ts-ignore
 		window.bringwidgets?.import.render(bringBtn, {
