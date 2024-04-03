7cff5d2f324e47e2a365a898dd1d4ab15bef188c
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2024-04-03 16:05:31 +0200

    Added Recipe Selection Modal

--- src/routes/planner/+page.svelte
+++ src/routes/planner/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+	import type { Recipe } from '../../models/Recipe';
+	import SelectRecipeModal from './SelectRecipeModal.svelte';
+
 	let days = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'];
 	let meals = ['Frühstück', 'Mittagessen', 'Abendessen', 'Snack'];
+
+	let selectRecipeModal: HTMLDialogElement;
+	function openSelectRecipeModal() {
+		selectRecipeModal.showModal();
+	}
+
+	function selectedHandler(recipe: Recipe) {
+		console.log(recipe);
+		selectRecipeModal.close();
+	}
 </script>
 
 <svelte:head>
@@ -15,8 +28,9 @@
 				{#each meals as meal}
 					<div class="flex flex-col gap-2">
 						<h3 class="text-md">{meal}</h3>
-						<div
+						<button
 							class="flex justify-center items-center cursor-pointer bg-base-200 rounded-lg w-full h-12 shadow-md shadow-neutral/50"
+							on:click={openSelectRecipeModal}
 						>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -28,10 +42,12 @@
 							>
 								<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 							</svg>
-						</div>
+						</button>
 					</div>
 				{/each}
 			</div>
 		</div>
 	{/each}
 </div>
+
+<SelectRecipeModal bind:modal={selectRecipeModal} {selectedHandler} />
