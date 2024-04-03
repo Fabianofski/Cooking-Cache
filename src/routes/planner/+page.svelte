bbf47cd383102e2c9f39e269837523713a417c4f
Fabian <friedrich.fabian@gmx.net>
2024-04-03 10:49:40 +0200

    Added first design draft for weekly planner

--- src/routes/planner/+page.svelte
+++ src/routes/planner/+page.svelte
@@ -1,4 +1,37 @@
+<script lang="ts">
+	let days = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'];
+	let meals = ['Frühstück', 'Mittagessen', 'Abendessen', 'Snack'];
+</script>
+
 <svelte:head>
 	<title>Wochenplaner | Cooking Cache</title>
 </svelte:head>
-Wochenplaner
+
+<div class="flex flex-col gap-4">
+	{#each days as day}
+		<div class="min-h-24">
+			<h2 class="text-lg font-bold">{day}</h2>
+			<div class="grid grid-cols-fluid gap-4">
+				{#each meals as meal}
+					<div class="flex flex-col gap-2">
+						<h3 class="text-md">{meal}</h3>
+						<div
+							class="flex justify-center items-center cursor-pointer bg-base-200 rounded-lg w-full h-12 shadow-md shadow-neutral/50"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+							</svg>
+						</div>
+					</div>
+				{/each}
+			</div>
+		</div>
+	{/each}
+</div>
