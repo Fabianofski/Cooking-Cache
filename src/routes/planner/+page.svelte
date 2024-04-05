e636ffbd30877f053723cd82a17168a4337235ed
Fabian Friedrich <68951860+Fabianofski@users.noreply.github.com>
2024-04-05 09:29:13 +0200

    Added btn to remove recipes from weekly plan

--- src/routes/planner/+page.svelte
+++ src/routes/planner/+page.svelte
@@ -38,6 +38,11 @@
 		selectRecipeModal.close();
 	}
 
+	function removeRecipeFromPlan(day: string, mealIndex: number) {
+		weeklyPlan[day].recipes.splice(mealIndex, 1);
+		weeklyPlan = { ...weeklyPlan };
+	}
+
 	function getRecipe(recipeId: string, collectionId: string) {
 		const recipes = $recipeCollectionsStore[collectionId].recipes;
 		return recipes.find((recipe) => recipe.id === recipeId);
@@ -62,8 +67,26 @@
 			<div class="grid grid-cols-fluid gap-4">
 				<div class="flex flex-col gap-2">
 					{#if weeklyPlan[day]}
-						{#each weeklyPlan[day].recipes as meal}
-							<SmallRecipeCard recipe={getRecipe(meal.recipeId, meal.collectionId)} />
+						{#each weeklyPlan[day].recipes as meal, index}
+							<div class="flex">
+								<SmallRecipeCard recipe={getRecipe(meal.recipeId, meal.collectionId)} />
+								<button class="btn h-full" on:click={() => removeRecipeFromPlan(day, index)}>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="w-5 h-5"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+										/>
+									</svg>
+								</button>
+							</div>
 						{/each}
 					{/if}
 					<button
@@ -76,7 +99,7 @@
 							viewBox="0 0 24 24"
 							stroke-width="1.5"
 							stroke="currentColor"
-							class="w-6 h-6"
+							class="w-4 h-4"
 						>
 							<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 						</svg>
