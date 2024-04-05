5d8876abec813422de8ab1b36ba8245e0db6b771
Fabian <friedrich.fabian@gmx.net>
2024-04-05 19:31:36 +0200

    Fixed grid layout in weekly planner

--- src/routes/planner/+page.svelte
+++ src/routes/planner/+page.svelte
@@ -109,64 +109,66 @@
 	</div>
 	<div class="grid grid-cols-fluid gap-4 w-full">
 		{#each days as day}
-			<div class="flex flex-col gap-2">
-				<h2 class="text-lg font-bold">
-					{new Date(day).toLocaleDateString('de-DE', {
-						weekday: 'long',
-						day: 'numeric',
-						month: 'numeric',
-						year: 'numeric'
-					})}
-				</h2>
-				{#if $weeklyPlanLoadingStore === 'LOADING'}
-					<SmallRecipeSkeleton />
-				{:else if weeklyPlan[day]}
-					{#each weeklyPlan[day].recipes as meal, index}
-						<div class="flex">
-							<a
-								class="w-full"
-								href={`/recipe/${generateShortCollectionId(
-									$recipeCollectionsStore[meal.collectionId],
-									$recipeCollectionsStore
-								)}/${generateShortRecipeId(
-									getRecipe(meal.recipeId, meal.collectionId),
-									$recipeCollectionsStore[meal.collectionId].recipes
-								)}`}
-							>
-								<SmallRecipeCard recipe={getRecipe(meal.recipeId, meal.collectionId)} />
-							</a>
-							<button
-								class="btn btn-ghost h-full"
-								on:click={() => removeRecipeFromPlan(day, index)}
-								disabled={loadingRemoval}
-							>
-								{#if loadingRemoval && removalIndex === index && removalDate === day}
-									<span class="loading-spinner loading loading-md" />
-								{:else}
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke-width="1.5"
-										stroke="currentColor"
-										class="w-5 h-5"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-										/>
-									</svg>
-								{/if}
-							</button>
-						</div>
-					{/each}
-					{#if loadingAdd && selectedDay === day}
-						<div class="flex">
-							<SmallRecipeSkeleton />
-						</div>
+			<div class="flex flex-col gap-2 justify-between">
+				<div class="flex flex-col gap-2">
+					<h2 class="text-lg font-bold">
+						{new Date(day).toLocaleDateString('de-DE', {
+							weekday: 'long',
+							day: 'numeric',
+							month: 'numeric',
+							year: 'numeric'
+						})}
+					</h2>
+					{#if $weeklyPlanLoadingStore === 'LOADING'}
+						<SmallRecipeSkeleton />
+					{:else if weeklyPlan[day]}
+						{#each weeklyPlan[day].recipes as meal, index}
+							<div class="flex">
+								<a
+									class="w-full"
+									href={`/recipe/${generateShortCollectionId(
+										$recipeCollectionsStore[meal.collectionId],
+										$recipeCollectionsStore
+									)}/${generateShortRecipeId(
+										getRecipe(meal.recipeId, meal.collectionId),
+										$recipeCollectionsStore[meal.collectionId].recipes
+									)}`}
+								>
+									<SmallRecipeCard recipe={getRecipe(meal.recipeId, meal.collectionId)} />
+								</a>
+								<button
+									class="btn btn-ghost h-full"
+									on:click={() => removeRecipeFromPlan(day, index)}
+									disabled={loadingRemoval}
+								>
+									{#if loadingRemoval && removalIndex === index && removalDate === day}
+										<span class="loading-spinner loading loading-md" />
+									{:else}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke-width="1.5"
+											stroke="currentColor"
+											class="w-5 h-5"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+											/>
+										</svg>
+									{/if}
+								</button>
+							</div>
+						{/each}
+						{#if loadingAdd && selectedDay === day}
+							<div class="flex">
+								<SmallRecipeSkeleton />
+							</div>
+						{/if}
 					{/if}
-				{/if}
+				</div>
 				<button
 					class="btn"
 					on:click={() => openSelectRecipeModal(day)}
