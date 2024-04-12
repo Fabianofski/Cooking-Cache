8a950892fec15977c04736b8f7f317da0dd358d0
Fabian <friedrich.fabian@gmx.net>
2024-04-12 16:56:02 +0200

    Added Small Recipe Collection Card to Select Modal

--- src/routes/SelectCollectionModal.svelte
+++ src/routes/SelectCollectionModal.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import SmallRecipeCollectionCard from '../components/SmallRecipeCollectionCard.svelte';
+	import SmallRecipeSkeleton from '../components/SmallRecipeSkeleton.svelte';
 	import type { RecipeCollection } from '../models/RecipeCollections';
 	import { recipeCollectionsStore } from '../stores/recipeCollectionsStore';
+	import { loadingStateStore } from '../stores/store';
 
 	export let modal: HTMLDialogElement;
 	export let selectedHandler: (recipe: RecipeCollection) => void;
@@ -10,15 +13,15 @@
 	<div class="modal-box flex flex-col gap-4 max-w-[30rem] px-4">
 		<h3 class="text-lg font-bold">Wähle eine Rezeptsammlung</h3>
 		<div class="flex flex-col gap-2 h-[34rem] overflow-y-auto">
-			{#each Object.values($recipeCollectionsStore) as collection}
-				<button
-					on:click={() => {
-						selectedHandler(collection);
-					}}
-				>
-					{collection.id}
-				</button>
-			{/each}
+			{#if $loadingStateStore !== 'FINISHED'}
+                {#each Array.from({ length: 5 }) as _}
+                    <SmallRecipeSkeleton />
+                {/each}
+			{:else}
+				{#each Object.values($recipeCollectionsStore) as collection}
+					<SmallRecipeCollectionCard recipeCollection={collection} clickHandler={selectedHandler} />
+				{/each}
+			{/if}
 		</div>
 		<form method="dialog">
 			<button class="btn btn-ghost btn-block">Abbrechen</button>
